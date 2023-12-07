1ed3f7fef0640b48547c26db8e39672181332b67
zjq <jiaqizhao122@sina.com>
2023-12-08 00:24:54 +0800

    zjq,feat:ignore

--- chatbot-api-application/src/main/java/cn/bugstack/chatbot/api/application/job/ChatbotTask.java
+++ chatbot-api-application/src/main/java/cn/bugstack/chatbot/api/application/job/ChatbotTask.java
@@ -13,12 +13,6 @@ import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Random;
 
-/**
- * @author 小傅哥，微信：fustack
- * @description 任务体
- * @github https://github.com/fuzhengwei
- * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- */
 public class ChatbotTask implements Runnable {
 
     private Logger logger = LoggerFactory.getLogger(ChatbotTask.class);
