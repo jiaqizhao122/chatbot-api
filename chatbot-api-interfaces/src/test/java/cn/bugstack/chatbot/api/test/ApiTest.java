c063e36a2bbfe5fd7b2bc3242336eb19adcef0fe
jiaqizhao122 <103194053+jiaqizhao122@users.noreply.github.com>
2023-12-25 14:43:08 +0800

    Update ApiTest.java

--- chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -15,12 +15,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
-/**
- * @author 小傅哥，微信：fustack
- * @description 单元测试
- * @github https://github.com/fuzhengwei
- * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- */
 public class ApiTest {
 
     @Test
